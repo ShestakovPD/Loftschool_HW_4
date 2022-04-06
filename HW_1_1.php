b9007d8378405b7748ce147e721822aaf4f71307
ShestakovPD <11pasha@mail.ru>
2022-04-06 15:20:35 +0500

    Домашнее задание №_1

---
+++ HW_1_1.php
@@ -0,0 +1,8 @@
+<?php
+
+$name = 'Pavel';
+$age = '45';
+
+echo("Меня зовут: $name."); ?><br><?php
+print("Мне $age лет."); ?><br><?php
+echo(" \"!|/'\"\ ");
---
+++ HW_1_2_config.php
@@ -0,0 +1,9 @@
+<?php
+
+const PICTURE = 80;
+const FELT_TIP_PEN = 23;
+const PENCIL = 40;
+const PAINTS = PICTURE - FELT_TIP_PEN - PENCIL;
+
+
+
